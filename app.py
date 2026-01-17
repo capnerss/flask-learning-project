b8712a964778d9deef17cc74902f79a35beef6f9
capnerss <capners29@gmail.com>
2026-01-17 22:42:27 +0200

    FL-8 Loo User mudel koos rolli väljaga

--- app.py
+++ app.py
@@ -29,12 +29,12 @@ db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
 
 # --- MODELI ---
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(20), default='User')
+class User(db.Model):#
+    id = db.Column(db.Integer, primary_key=True)#
+    username = db.Column(db.String(80), nullable=False)#
+    email = db.Column(db.String(120), unique=True, nullable=False)#
+    password_hash = db.Column(db.String(200), nullable=False)#
+    role = db.Column(db.String(20), default='User')#
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
