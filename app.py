62e95f975fa83b9cc29a0c9c754df410c828b5ed
capnerss <capners29@gmail.com>
2026-01-17 22:59:52 +0200

    FL-8 added comment for use model

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ class User(db.Model):#
     username = db.Column(db.String(80), nullable=False)#
     email = db.Column(db.String(120), unique=True, nullable=False)#
     password_hash = db.Column(db.String(200), nullable=False)#
-    role = db.Column(db.String(20), default='User')#
+    role = db.Column(db.String(20), default='User')# Super puper for jira
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
